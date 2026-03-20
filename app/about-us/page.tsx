65b6f9946efe42c1d26e5598537fb02885f668d3
Brian Tran <briant@fivestartaxhelp.com>
2026-03-20 08:42:55 -0700

    added coming soon placeholders

---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+    return (
+        <div className="flex items-center justify-center min-h-screen font-sans">
+            <h1>Coming Soon</h1>
+        </div>
+    );
+}
